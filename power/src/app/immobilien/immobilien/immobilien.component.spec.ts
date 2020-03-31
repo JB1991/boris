2f841402929c776d3995416fe7cc986ed75cdf96
Jan-Hendrik Hemmje <jan-hendrik.hemmje@lgln.niedersachsen.de>
2020-03-31 14:08:58 +0200

    Fixed tests for ImmobilienComponent

--- power/src/app/immobilien/immobilien/immobilien.component.spec.ts
+++ power/src/app/immobilien/immobilien/immobilien.component.spec.ts
@@ -1,14 +1,18 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import {ImmobilienComponent} from './immobilien.component';
 
 describe('ImmobilienComponent', () => {
   let component: ImmobilienComponent;
   let fixture: ComponentFixture<ImmobilienComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ImmobilienComponent ]
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
@@ -17,6 +21,9 @@ describe('ImmobilienComponent', () => {
     fixture = TestBed.createComponent(ImmobilienComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create', () => {
