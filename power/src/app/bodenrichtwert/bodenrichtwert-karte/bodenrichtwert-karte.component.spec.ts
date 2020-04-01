57571c99a9fc99d80193f3b1c2e39ca25ed8e58c
Jan-Hendrik Hemmje <jan-hendrik.hemmje@lgln.niedersachsen.de>
2020-04-01 09:50:41 +0200

    Fixed tests for BodenrichtwertkarteComponent

--- power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.spec.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.spec.ts
@@ -1,14 +1,19 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {HttpClient} from '@angular/common/http';
 
 import {BodenrichtwertKarteComponent} from './bodenrichtwert-karte.component';
 
 describe('BodenrichtwertkarteComponent', () => {
   let component: BodenrichtwertKarteComponent;
   let fixture: ComponentFixture<BodenrichtwertKarteComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [BodenrichtwertKarteComponent]
+      declarations: [BodenrichtwertKarteComponent],
+      imports: [ HttpClientTestingModule ]
     })
       .compileComponents();
   }));
@@ -16,7 +21,11 @@ describe('BodenrichtwertkarteComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BodenrichtwertKarteComponent);
     component = fixture.componentInstance;
+    component.teilmarkt = '';
     fixture.detectChanges();
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create', () => {
