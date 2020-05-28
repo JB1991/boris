dc141861e99a1df989e213aa677a6318eb4b2344
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-05-28 18:26:48 +0200

    Added formular landing page

--- power/src/app/fragebogen/home/home.component.ts
+++ power/src/app/fragebogen/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'power-formulars-home',
@@ -8,10 +9,14 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, private router: Router) {
     this.titleService.setTitle('LGLN - POWER.NI');
   }
 
   ngOnInit() {
+  }
+
+  submitFormular(value: string) {
+    this.router.navigate(['formulare', 'fillout', value], { replaceUrl: true });
   }
 }
