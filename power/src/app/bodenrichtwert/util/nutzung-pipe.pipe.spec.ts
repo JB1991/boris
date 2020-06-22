dc5e12cce38e518a04d7c95d69cfa8476ea19cfc
Richard Bischof <richard.bischof@lgln.niedersachsen.de>
2020-06-22 12:35:26 +0200

    added pipe

---
+++ power/src/app/bodenrichtwert/util/nutzung-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NutzungPipePipe } from './nutzung-pipe.pipe';
+
+describe('NutzungPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new NutzungPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
