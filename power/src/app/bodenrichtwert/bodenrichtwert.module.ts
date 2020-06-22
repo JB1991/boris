09e2eca1bd7e46c07e99838c70ff88bccc7f41fe
Richard Bischof <richard.bischof@lgln.niedersachsen.de>
2020-06-22 11:07:25 +0200

    fixed import

--- power/src/app/bodenrichtwert/bodenrichtwert.module.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert.module.ts
@@ -14,6 +14,7 @@ import {AngularResizedEventModule} from 'angular-resize-event';
 import {NgxMapboxGLModule} from 'ngx-mapbox-gl';
 
 import {NgxEchartsModule} from 'ngx-echarts';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import {NgxEchartsModule} from 'ngx-echarts';
     NgxEchartsModule,
     AngularResizedEventModule,
     BodenrichtwertRoutingModule,
+    RouterModule.forRoot([]),
   ],
   providers: [BodenrichtwertService]
 })
