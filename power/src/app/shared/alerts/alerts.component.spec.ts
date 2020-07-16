bbdf0fe844f8ab9362c51820ed1e6e18f8974c39
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-07-16 09:23:07 +0200

    Added tests for alerts and loadingscreen

--- power/src/app/shared/alerts/alerts.component.spec.ts
+++ power/src/app/shared/alerts/alerts.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertsComponent } from './alerts.component';
+import { AlertsService } from './alerts.service';
 
-describe('Fragebogen.Alerts.AlertsComponent', () => {
+describe('Shared.Alerts.AlertsComponent', () => {
   let component: AlertsComponent;
   let fixture: ComponentFixture<AlertsComponent>;
+  let storage: AlertsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AlertsComponent ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(AlertsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+    storage = TestBed.inject(AlertsService);
+    expect(storage.alertslist).toEqual([]);
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(storage.alertslist).toEqual([]);
+  });
+  it('should close one alert', () => {
+    storage.NewAlert('success', 'Test Title 1', 'Test Body');
+    storage.NewAlert('danger', 'Test Title 2', 'Test Body', 3500);
+    storage.NewAlert('info', 'Test Title 3', 'Test Body', 4000);
+
+    component.onClosed(1); // delete danger alert
+    expect(storage.alertslist.length).toEqual(2);
+    expect(storage.alertslist[0].type).toEqual('success');
+    expect(storage.alertslist[1].type).toEqual('info');
+  });
+
+  // reset service after each test
+  afterEach(() => {
+    storage.resetService();
+    storage = null;
   });
 });
--- power/src/app/shared/loadingscreen/loadingscreen.service.spec.ts
+++ power/src/app/shared/loadingscreen/loadingscreen.service.spec.ts
@@ -17,5 +17,19 @@ describe('Fragebogen.Loadingscreen.LoadingscreenService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    expect(service.isVisible).toBeFalsy();
+  });
+  it('should change visibility', () => {
+    service.setVisible(true);
+    expect(service.isVisible).toBeTruthy();
+    service.setVisible(false);
+    expect(service.isVisible).toBeFalsy();
+  });
+  it('should reset alert.service', () => {
+    service.setVisible(true);
+
+    // reset service
+    service.resetService();
+    expect(service.isVisible).toBeFalsy();
   });
 });
