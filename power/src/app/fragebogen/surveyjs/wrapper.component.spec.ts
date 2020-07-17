48caeb15f1993fe45ba7623615f97b2471440659
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-07-17 09:10:28 +0200

    Added module and surveyjs tests

--- power/src/app/fragebogen/surveyjs/wrapper.component.spec.ts
+++ power/src/app/fragebogen/surveyjs/wrapper.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WrapperComponent } from './wrapper.component';
+import { Bootstrap4_CSS } from './style';
 
 describe('Fragebogen.Surveyjs.WrapperComponent', () => {
   let component: WrapperComponent;
@@ -21,6 +22,30 @@ describe('Fragebogen.Surveyjs.WrapperComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should do surveyjs', () => {
+    component.model = {'title': 'Hallo Welt', 'data': 5};
+    component.mode = 'edit';
+    component.theme = 'bootstrap';
+    component.css = Bootstrap4_CSS;
+    component.completedHtml = 'Test';
+    component.navigateToURL = 'https://google.com/';
+    component.showInvisible = true;
+    spyOn(component.changes, 'emit');
+    spyOn(component.interimResult, 'emit');
+    spyOn(component.submitResult, 'emit');
+
     component.ngOnChanges();
+    fixture.detectChanges();
+
+    expect(component.model).toEqual({'title': 'Hallo Welt', 'data': 5});
+    expect(component.css).toEqual(Bootstrap4_CSS);
+    expect(component.completedHtml).toEqual('Test');
+    expect(component.showInvisible).toBeTrue();
+    expect(component.submitResult).toBeTruthy();
+
+    expect(component.props['data']).toEqual(5);
+    expect(component.props['model']['mode']).toEqual('edit');
   });
 });
