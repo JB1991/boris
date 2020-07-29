3b8a4b71ba1c1810bbb09822d1e37b8a532832f0
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-07-29 16:57:32 +0200

    New keycloak client

--- power/src/environments/environment.prod.ts
+++ power/src/environments/environment.prod.ts
@@ -6,8 +6,8 @@ export const environment = {
   basemap : 'https://vector-tiles-lgln.s3.ap.cloud-object-storage.appdomain.cloud/basemap.json',
   formAPI: '/formapi/',
   auth: {
-    clientid: 'frontend',
-    clientsecret: '4493f922-9f3e-416f-8891-91bd52300378',
+    clientid: 'power',
+    clientsecret: 'f53dc4b4-42fa-40bc-8ce3-63760a994b88',
     authurl: 'https://keycloak.power-cluster-65655d4c73bf47a3300821aa2939abf4-0001.eu-de.containers.appdomain.cloud/auth/realms/power/protocol/openid-connect/auth',
     tokenurl: 'https://keycloak.power-cluster-65655d4c73bf47a3300821aa2939abf4-0001.eu-de.containers.appdomain.cloud/auth/realms/power/protocol/openid-connect/token',
     introspecturl: 'https://keycloak.power-cluster-65655d4c73bf47a3300821aa2939abf4-0001.eu-de.containers.appdomain.cloud/auth/realms/power/protocol/openid-connect/token/introspect'
