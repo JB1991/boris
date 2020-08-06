b0d9d2acdd2d198d691911fcef2133569a1c9b54
Jan-Hendrik Hemmje <jan-hendrik.hemmje@lgln.niedersachsen.de>
2020-08-06 14:41:46 +0200

    German formatting of numbers

--- power/src/app/app.module.ts
+++ power/src/app/app.module.ts
@@ -1,15 +1,19 @@
-import {APP_INITIALIZER, NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CommonModule} from '@angular/common';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {Config, ConfigService} from '@app/config.service';
-import {catchError, map} from 'rxjs/operators';
-import {Observable, ObservableInput, of} from 'rxjs';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Config, ConfigService } from '@app/config.service';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, ObservableInput, of } from 'rxjs';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { environment } from '@env/environment';
+
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+registerLocaleData(localeDe);
 
 import { AuthModule } from '@app/shared/auth/auth.module';
 import { AlertsModule } from '@app/shared/alerts/alerts.module';
@@ -24,7 +28,7 @@ export function load(httpClient: HttpClient, configService: ConfigService) {
             configService.config = x;
             resolve(true);
           }),
-          catchError((x: {status: number}, caught: Observable<void>): ObservableInput<{}> => {
+          catchError((x: { status: number }, caught: Observable<void>): ObservableInput<{}> => {
             console.error('could not load config.json');
             if (x.status !== 404) {
               resolve(false);
@@ -61,7 +65,11 @@ export function load(httpClient: HttpClient, configService: ConfigService) {
         HttpClient,
         ConfigService
       ]
-    }
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'de-DE'
+    },
   ],
   bootstrap: [AppComponent]
 })
