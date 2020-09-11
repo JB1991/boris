ec61f6af2b03be267d64ea861351292dd1418881
Malte Wrogemann <wroge.malte@gmail.com>
2020-09-11 09:42:29 +0200

    update tests

--- power/src/app/fragebogen/public-dashboard/public-dashboard.component.spec.ts
+++ power/src/app/fragebogen/public-dashboard/public-dashboard.component.spec.ts
@@ -9,13 +9,11 @@ import { AlertsService } from '@app/shared/alerts/alerts.service';
 import { LoadingscreenService } from '@app/shared/loadingscreen/loadingscreen.service';
 import { PublicDashboardComponent } from './public-dashboard.component';
 import { FormAPIService } from '../formapi.service';
-import { environment } from '@env/environment';
+import { throwError } from 'rxjs';
 
 describe('Fragebogen.PublicDashboard.DashboardComponent', () => {
     let component: PublicDashboardComponent;
     let fixture: ComponentFixture<PublicDashboardComponent>;
-    let httpClient: HttpClient;
-    let httpTestingController: HttpTestingController;
 
     const publicForms = require('../../../assets/fragebogen/public-get-forms.json');
     const publicFormsBig = require('../../../assets/fragebogen/public-get-forms-big.json');
@@ -43,11 +41,9 @@ describe('Fragebogen.PublicDashboard.DashboardComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
 
-        spyOn(console, 'log');
+        // spyOn(console, 'log');
         spyOn(component.router, 'navigate');
         spyOn(component.alerts, 'NewAlert');
-        httpClient = TestBed.inject(HttpClient);
-        httpTestingController = TestBed.inject(HttpTestingController);
     }));
 
     it('should be created', () => {
@@ -59,43 +55,52 @@ describe('Fragebogen.PublicDashboard.DashboardComponent', () => {
     */
     it('should succeed', (done) => {
         spyOn(component.formAPI, 'getPublicForms').and.returnValue(Promise.resolve(publicForms));
-        component.filterByTitle('something');
-        component.changePage(1);
-        component.changePerPage(1);
-        component.changeSort('title');
+        component.title = 'something';
         component.update().then(() => {
-            done();
             expect(component.data).toBe(publicForms.data);
             expect(component.total).toBe(publicForms.total);
-            expect(component.title).toBe('something');
+            done();
         });
     });
 
     it('should succeed', (done) => {
         spyOn(component.formAPI, 'getPublicForms').and.returnValue(Promise.resolve(publicFormsBig));
-        component.filterByTitle('something');
-        component.changePage(0);
-        component.changePerPage(10);
-        component.changeSort('published');
         component.update().then(() => {
-            done();
             expect(component.data).toBe(publicFormsBig.data);
             expect(component.total).toBe(publicFormsBig.total);
-            expect(component.perPage).toBe(10);
+            done();
         });
     });
+
+    it('should succeed', (done) => {
+        spyOn(component, 'update');
+        component.changePage(1);
+        component.changePerPage(1);
+        component.filterByTitle('something');
+        component.changeSort('published');
+        expect(component.page).toBe(1);
+        expect(component.perPage).toBe(1);
+        expect(component.title).toBe('something');
+        expect(component.sort).toBe('published');
+        expect(component.order).toBe('asc');
+        component.changeSort('published');
+        expect(component.sort).toBe('published');
+        expect(component.order).toBe('desc');
+        done();
+    })
 
     /*
         Error
     */
     it('should fail', (done) => {
-        spyOn(component.formAPI, 'getPublicForms').and.throwError('fail');
-        component.filterByTitle('something');
+        spyOn(component.formAPI, 'getPublicForms').and.callFake(() => {
+            return Promise.reject(new Error('fail'));
+        });
         component.update().then(() => {
-            done();
             expect(component.alerts.NewAlert).toHaveBeenCalledTimes(1);
             expect(component.alerts.NewAlert)
-                .toHaveBeenCalledWith('danger', 'Laden fehlgeschlagen', 'fail');
+                .toHaveBeenCalledWith('danger', 'Laden fehlgeschlagen', 'Error: fail');
+            done();
         });
     });
 });
