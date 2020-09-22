bff0aa4ab664bda5220037683f3921dbbb2e5879
Jan-Hendrik Hemmje <jan-hendrik.hemmje@lgln.niedersachsen.de>
2020-09-22 09:19:03 +0200

    Unit tests for Bodenrichtwert.Pipes.BeitragPipe

--- power/src/app/bodenrichtwert/pipes/beitrag.pipe.spec.ts
+++ power/src/app/bodenrichtwert/pipes/beitrag.pipe.spec.ts
@@ -1,9 +1,22 @@
 import { BeitragPipe } from './beitrag.pipe';
 
 describe('Bodenrichtwert.Pipes.BeitragPipe', () => {
+    let pipe: BeitragPipe;
+
+    beforeEach(() => {
+        pipe = new BeitragPipe();
+    });
+
     it('create an instance', () => {
-        const pipe = new BeitragPipe();
         expect(pipe).toBeTruthy();
+    });
+
+    it('should return the correct value', () => {
+        expect(pipe.transform('1')).toEqual('erschließungsbeitrags- und kostenerstattungsbetragsfrei');
+    });
+
+    it('should return undefined when there is no appropriate entry', () => {
+        expect(pipe.transform('0')).toEqual(undefined);
     });
 });
 /* vim: set expandtab ts=4 sw=4 sts=4: */
