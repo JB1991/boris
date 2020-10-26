422302a85771cbaf9adfc73bc9f9c7b32c7d12fa
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-10-26 18:17:19 +0100

    Added inlcudes method to polyfills

--- power/src/polyfills.ts
+++ power/src/polyfills.ts
@@ -59,3 +59,16 @@ import '@angular/localize/init';
 
 // aws-sdk requires global to exist
 (window as any).global = window;
+
+// support for includes on IE/Edge
+if (!Array.prototype.includes) {
+    Object.defineProperty(Array.prototype, "includes", {
+        enumerable: false,
+        value: function (obj) {
+            var newArr = this.filter(function (el) {
+                return el === obj;
+            });
+            return newArr.length > 0;
+        }
+    });
+}
