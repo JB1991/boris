898194519bb440b259fdc394ea697e1f5de1e0de
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2020-10-29 14:10:59 +0100

    Added minimodal-footer directive

---
+++ power/src/app/shared/modalmini/modalmini-footer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ModalminiFooterDirective } from './modalmini-footer.directive';
+
+describe('Shared.ModalminiFooterDirective', () => {
+    it('should create an instance', () => {
+        const directive = new ModalminiFooterDirective(null);
+        expect(directive).toBeTruthy();
+    });
+});
