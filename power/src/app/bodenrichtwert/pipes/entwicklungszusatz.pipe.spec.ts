718f4fb59b0c356ede24da814927b53caddca338
NilsKirsch <nilschristopher.kirsch@lgln.niedersachsen.de>
2020-11-09 12:56:48 +0100

    add tests to pipes

--- power/src/app/bodenrichtwert/pipes/entwicklungszusatz.pipe.spec.ts
+++ power/src/app/bodenrichtwert/pipes/entwicklungszusatz.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { EntwicklungszusatzPipe } from './entwicklungszusatz.pipe';
 
 describe('EntwicklungszusatzPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EntwicklungszusatzPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: EntwicklungszusatzPipe;
+
+    beforeEach(() => {
+        pipe = new EntwicklungszusatzPipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('should return the correct value', () => {
+        expect(pipe.transform('SU')).toEqual('Sanierungsbeeinflusster Bodenrichtwert, ohne Berücksichtigung der rechtlichen und tatsächlichen Neuordnung');
+    });
+
+    it('should return undefined when there is no appropriate entry', () => {
+        expect(pipe.transform('NULL')).toEqual(undefined);
+    });
 });
--- power/src/app/bodenrichtwert/pipes/entwicklungszustand.pipe.spec.ts
+++ power/src/app/bodenrichtwert/pipes/entwicklungszustand.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { EntwicklungszustandPipe } from './entwicklungszustand.pipe';
 
 describe('EntwicklungszustandPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EntwicklungszustandPipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: EntwicklungszustandPipe;
+
+    beforeEach(() => {
+        pipe = new EntwicklungszustandPipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('should return the correct value', () => {
+        expect(pipe.transform('B')).toEqual('Baureifes Land');
+    });
+
+    it('should return undefined when there is no appropriate entry', () => {
+        expect(pipe.transform('NULL')).toEqual(undefined);
+    });
 });
