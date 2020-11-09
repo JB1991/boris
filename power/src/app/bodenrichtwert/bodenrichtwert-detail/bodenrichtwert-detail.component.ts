b1664bcdc5ab9f3843a3ec92c55dbbea21e27c77
Jan-Hendrik Hemmje <jan-hendrik.hemmje@lgln.niedersachsen.de>
2020-11-09 11:13:54 +0100

    Show available Umrechnungen in details

--- power/src/app/bodenrichtwert/bodenrichtwert-detail/bodenrichtwert-detail.component.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert-detail/bodenrichtwert-detail.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'power-bodenrichtwert-detail',
     templateUrl: './bodenrichtwert-detail.component.html',
     styleUrls: ['./bodenrichtwert-detail.component.css'],
 })
-export class BodenrichtwertDetailComponent {
+export class BodenrichtwertDetailComponent implements OnInit {
 
     brzStrings = {
         'brz': $localize`Bodenrichtwertzone`,
+        'conversion': $localize`Umrechnung`,
         'state' : $localize`Beitragsabgabenrechtlicher Zustand`,
         'usage': $localize`Art der Nutzung`,
         'value': $localize`Bodenrichtwert`
     };
 
     @Input() feature: any;
+
+    ngOnInit() {
+    }
 
 }
 
