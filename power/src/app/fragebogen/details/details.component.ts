c50b2c6ec9793caa482a88ed5a92f88807d3c737
Malte Wrogemann <wroge.malte@gmail.com>
2020-11-20 11:23:27 +0100

    archivieren/löschen button anzeigeregel

--- power/src/app/fragebogen/details/details.component.ts
+++ power/src/app/fragebogen/details/details.component.ts
@@ -131,6 +131,7 @@ export class DetailsComponent implements OnInit {
                 return;
             }
             await this.formapi.updateForm(this.form.id, { status: 'cancelled' });
+            this.form.status = 'cancelled'
             this.alerts.NewAlert('success', $localize`Formular archiviert`,
                 $localize`Das Formular wurde erfolgreich archiviert.`);
         } catch (error) {
