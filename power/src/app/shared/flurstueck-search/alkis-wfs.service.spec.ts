31a54df8198c5cfddeb0dbd4a8d7f1d79459e777
NilsKirsch <nilschristopher.kirsch@lgln.niedersachsen.de>
2020-12-01 09:28:21 +0100

    fixed wfs test

--- power/src/app/shared/flurstueck-search/alkis-wfs.service.spec.ts
+++ power/src/app/shared/flurstueck-search/alkis-wfs.service.spec.ts
@@ -9,7 +9,7 @@ describe('Shared.Flurstueck-search.AlkisWfsService', () => {
     const fst: Flurstueck = require('../../../assets/boden/flurstueck-search-samples/flurstueck.json');
     const xmlData: any = XmlParser.parse('../../../assets/boden/flurstueck-search-samples/xml-data.xml');
 
-    const url = '/wfs?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&STOREDQUERY_ID=FstFsk&FSK=035328003000790001__';
+    const url = '/wfs/alkis?REQUEST=GetFeature&SERVICE=WFS&VERSION=2.0.0&STOREDQUERY_ID=FstFsk&FSK=035328003000790001__';
 
     let service: AlkisWfsService;
     let httpController: HttpTestingController;
