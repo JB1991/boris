34fb1cc734385f74bfc728dc52e0b1c9a4579659
NilsKirsch <nilschristopher.kirsch@lgln.niedersachsen.de>
2020-12-10 10:20:30 +0100

    updated beispielrechnung umrechnungskoeffizient

--- power/src/app/bodenrichtwert/bodenrichtwert-detail/umrechnung/umrechnung.component.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert-detail/umrechnung/umrechnung.component.ts
@@ -25,26 +25,58 @@ export class UmrechnungComponent implements OnInit {
         ]
     };
     @Input() public brw: number;
+    @Input() public actualValue: any;
 
     strings = {
         'koef': $localize`Umrechnungskoeffizient`,
     };
 
-    einflussgroesse: string;
-    objectId: string;
+    public einflussgroesse: string;
+    public objectId: string;
+    public actualKoef: any;
 
     constructor(private einflussgroessePipe: EinflussgroessePipe,
-                private objectIdPipe: ObjectIdPipe) {
+        private objectIdPipe: ObjectIdPipe) {
     }
 
     ngOnInit(): void {
         this.einflussgroesse = this.einflussgroessePipe.transform(this.data.text);
         this.objectId = this.objectIdPipe.transform(this.data.objid);
         this.data.werte = this.sortBezugswerte(this.data.werte);
+        this.actualKoef = this.findActualKoef(this.data.werte);
     }
 
     sortBezugswerte(array) {
         return array.sort((a, b) => a.bzwt - b.bzwt);
+    }
+
+    private findActualKoef(werte: any) {
+        let val: any;
+        if (this.data.text === 'Art der Bebauung') {
+            if (this.actualValue.toString() === 'EFH') {
+                val = werte.find((v: any) => v.bzwt === 1);
+                if (val) {
+                    return val.koef
+                }
+            } else if (this.actualValue.toString() === 'MFH') {
+                val = werte.find((v: any) => v.bzwt === 2);
+                if (val) {
+                    return val.koef
+                }
+            }
+        } else if (this.data.text === 'FLAE') {
+            let roundedVal = Math.round(this.actualValue / 100) * 100;
+            val = werte.find((v: any) => v.bzwt.toString() === roundedVal.toString());
+            if (val) {
+                return val.koef
+            }
+        } else {
+            val = werte.find((v: any) => v.bzwt === this.actualValue);
+            if (val) {
+                return val.koef
+            }
+        }
+        return val;
     }
 
     public open() {
