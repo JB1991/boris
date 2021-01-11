107f7ef8075bbb0d901cf9149a83766faf389b22
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2021-01-11 16:36:56 +0100

    Fix IE not working

--- power/src/polyfills.ts
+++ power/src/polyfills.ts
@@ -21,6 +21,7 @@
 /** IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
 import 'core-js/es/array'; // required for Array.includes() and Array.form()
+import 'core-js/es/object/values';
 import 'core-js/features/url-search-params'; // required for URLSearchParams
 
 /**
