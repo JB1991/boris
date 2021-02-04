aa59a344f9660233c5c91e384730a8c8ba875111
Malte Wrogemann <wroge.malte@gmail.com>
2021-02-04 14:42:39 +0100

    fix: boris: change brw on stichtag change

--- power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.ts
@@ -580,7 +580,12 @@ export class BodenrichtwertKarteComponent implements OnInit, OnChanges {
         this.stichtag = stichtag;
         this.stichtagChange.next(stichtag);
 
-        this.flyTo(this.lat, this.lng);
+        if (!this.marker.getLngLat()) {
+            const center = this.map.getCenter();
+            this.flyTo(center.lat, center.lng);
+        } else {
+            this.flyTo(this.lat, this.lng);
+        }
         this.changeURL();
     }
 
@@ -618,7 +623,12 @@ export class BodenrichtwertKarteComponent implements OnInit, OnChanges {
             this.getBodenrichtwertzonen(this.lat, this.lng, this.teilmarkt.value);
         }
 
-        this.flyTo(this.lat, this.lng);
+        if (!this.marker.getLngLat()) {
+            const center = this.map.getCenter();
+            this.flyTo(center.lat, center.lng);
+        } else {
+            this.flyTo(this.lat, this.lng);
+        }
         this.changeURL();
     }
 
