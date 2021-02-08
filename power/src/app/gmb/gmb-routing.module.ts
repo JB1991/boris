0e067ec8958edc13d78bd63273a8c5a431020fc1
Karsten <karsten@nerdbox.de>
2021-02-08 08:39:32 +0100

    Initial commit of new GMB-Module

---
+++ power/src/app/gmb/gmb-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GmbComponent } from './gmb/gmb.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: GmbComponent,
+        children: [
+        ]
+    }];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class GmbRoutingModule { }
+
+/* vim: set expandtab ts=4 sw=4 sts=4: */
---
+++ power/src/app/gmb/gmb/gmb.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GmbComponent } from './gmb.component';
+
+describe('GmbComponent', () => {
+  let component: GmbComponent;
+  let fixture: ComponentFixture<GmbComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GmbComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GmbComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
