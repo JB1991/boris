9c955fe67cb7e7516abbe6400592a6370213bcd7
Karsten <karsten@nerdbox.de>
2021-02-12 09:15:51 +0100

    GMB: Fix tests

--- power/src/app/gmb/gmb/gmb.component.spec.ts
+++ power/src/app/gmb/gmb/gmb.component.spec.ts
@@ -1,13 +1,42 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GmbComponent } from './gmb.component';
+
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts';
+
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import {
+    download,
+    plusCircle,
+    dashCircle,
+    infoCircle
+} from 'ngx-bootstrap-icons';
+
+// Select some icons (use an object, not an array)
+/* eslint-disable object-shorthand */
+const icons = {
+    download,
+    plusCircle,
+    dashCircle,
+    infoCircle
+};
+/* eslint-enable object-shorthand */
+
 
 describe('GmbComponent', () => {
     let component: GmbComponent;
     let fixture: ComponentFixture<GmbComponent>;
+    let httpTestingController: HttpTestingController;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                NgxBootstrapIconsModule.forRoot(icons),
+                NgxEchartsModule.forRoot({ echarts }) // eslint-disable-line object-shorthand
+            ],
             declarations: [ GmbComponent ]
         })
             .compileComponents();
