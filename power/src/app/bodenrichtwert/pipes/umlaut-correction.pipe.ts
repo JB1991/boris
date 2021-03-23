9e2fdcc95d131218e7311220258aad43524e5094
JB.LGLN <jared.busch@lgln.niedersachsen.de>
2021-03-23 13:07:55 +0100

    fixed test

--- power/src/app/bodenrichtwert/pipes/umlaut-correction.pipe.ts
+++ power/src/app/bodenrichtwert/pipes/umlaut-correction.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class UmlautCorrectionPipe implements PipeTransform {
 
     corrections: Map<string, string> = new Map([
-        ['Produktion, Spedition', 'Produktion, Spedition'],
         ['Buero, hochw. Dienste', 'Büro, hochw. Dienste'],
         ['Handel, Geschaefte', 'Handel, Geschäfte'],
         ['Verbrauchermaerkte', 'Verbrauchermärkte'],
