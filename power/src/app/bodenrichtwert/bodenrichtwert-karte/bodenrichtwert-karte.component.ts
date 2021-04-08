d5203503eadbff2e5c3a1f24655f1fa32da79b37
Laurent Skiba <laurent.skiba@lgln.niedersachsen.de>
2021-04-08 11:08:45 +0200

    Added transformrequest

--- power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.ts
+++ power/src/app/bodenrichtwert/bodenrichtwert-karte/bodenrichtwert-karte.component.ts
@@ -515,6 +515,16 @@ export class BodenrichtwertKarteComponent implements OnChanges {
     }
 
     /**
+     * transformRequest
+     */
+    public transformRequest(url, resourceType) {
+        if (!url.startsWith('http') && resourceType === 'Tile') {
+            return { url: location.protocol + '//' + location.host + url };
+        }
+        return { url: url };
+    }
+
+    /**
      * dynamicLabelling
      * @param labelData
      * @param layerNames
